72063975913fb08878750c0fdadf6e485212ca42
Saksham <sakshamloona9@gmail.com>
2025-02-12 20:15:29 +0530

    Completely applied addSales Logic

--- StoreWise/addSales.vb
+++ StoreWise/addSales.vb
@@ -1,16 +1,19 @@
 ﻿Public Class addSales
 
     Private Sub phone_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles phone.KeyPress
-        If (Not Char.IsNumber(e.KeyChar) And Not e.KeyChar = Chr(Keys.Back) And Not e.KeyChar = Chr(Keys.Delete)) Then
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not e.KeyChar = Chr(Keys.Back) Then
             e.Handled = True
-            MessageBox.Show("Price must be in Numbers.")
+            MessageBox.Show("Phone must be in Numbers.", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
         End If
     End Sub
 
     Private Sub itemQuantity_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles itemQuantity.KeyPress
-        If (Not Char.IsNumber(e.KeyChar) And Not e.KeyChar = Chr(Keys.Back) And Not e.KeyChar = Chr(Keys.Delete)) Then
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not e.KeyChar = "."c AndAlso Not e.KeyChar = Chr(Keys.Back) Then
             e.Handled = True
-            MessageBox.Show("Quantity must be in Numbers.")
+            MessageBox.Show("Quantity must be in Numbers.", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        ElseIf e.KeyChar = "."c AndAlso itemQuantity.Text.Contains(".") Then
+            e.Handled = True
+            MessageBox.Show("Only one decimal point is allowed.", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
         End If
     End Sub
 
@@ -28,18 +31,10 @@
 
     Private Sub save_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles save.Click
         If (itemName.Text.Trim = "" Or itemQuantity.Text.Trim = "" Or customerName.Text.Trim = "" Or phone.Text.Trim = "" Or itemQuantity.Text.Trim = "") Then
-            MessageBox.Show("Please Fill All Fields")
+            MessageBox.Show("Please Fill All Fields", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
         Else
 
         End If
-    End Sub
-
-    Private Sub GroupBox1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs)
-
-    End Sub
-
-    Private Sub SalesPage_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
     End Sub
 
     Private Sub Add_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Add.Click
@@ -48,17 +43,42 @@
                 Dim price As Single
                 Dim quantity As Integer
                 Dim fAmt As Single
-                Dim diss As Single
-                itemList.Items.Add(itemName.Text)
+                Dim fAmount As String
+                Dim finalAmount As Single
+                Dim gst As Single
+
+
+                itemList.Items.Add(itemName.Text)   'Quantity and Price Logic
                 quantityList.Items.Add(itemQuantity.Text)
                 priceList.Items.Add("100")
                 price = CSng(priceList.Items.Item(priceList.Items.Count - 1))
-                quantity = CInt(quantityList.Items.Item(quantityList.Items.Count - 1))
-                fAmt = price * quantity
-                totalPrice.Items.Add(fAmt)
 
+
+                quantity = CInt(quantityList.Items.Item(quantityList.Items.Count - 1))  'Quantity Logic
+
+
+                gst = ((price * 18) / 100)       'GST Logic
+                gstAmt.Items.Add(gst)
+
+
+
+                fAmt = (price + gst) * quantity'Total Amount Logic
+                totalPrice.Items.Add(fAmt)
                 itemQuantity.Text = ""
                 itemName.SelectedIndex = 0
+
+
+
+                fAmount = fBill.Text            'Final Bill Amount Logic
+                fAmount = fAmount.Replace("₹"c, "")
+                fAmount = fAmount.Replace("/"c, "")
+                fAmount = fAmount.Replace("-"c, "")
+                finalAmount = CSng(fAmount)
+                finalAmount += fAmt
+                fBill.Text = "₹" & finalAmount & "/-"
+                If Discount.Text <> "" Then
+                    Discount_Cal()
+                End If
             Else
                 MessageBox.Show("Quantity Can't be Zero or Negative", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Error)
             End If
@@ -74,17 +94,82 @@
             If (ind < 0) Then
                 MessageBox.Show("Item list is empty.", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Else
+                Dim fAmount As String
+                Dim finalAmount As Single
+                Dim fAmt As Single
+                Dim gst As Single
+                Dim price As Single
+                Dim disc As Single
+                Dim quantity As Single
+
+                quantity = CSng(quantityList.Items.Item(ind))
+
+                gst = CSng(gstAmt.Items.Item(ind))
+                price = CSng(priceList.Items.Item(ind))
+                If Discount.Text <> "" Then
+                    disc = CSng(Discount.Text)
+                Else
+                    disc = 0
+                End If
+
+
+                fAmount = fBill.Text            'Final Bill Amount Logic
+                fAmount = fAmount.Replace("₹"c, "")
+                fAmount = fAmount.Replace("/"c, "")
+                fAmount = fAmount.Replace("-"c, "")
+                finalAmount = CSng(fAmount)
+                fAmt = finalAmount - ((price + gst - (((price + gst) * disc) / 100)) * quantity)
+                finalAmount = fAmt
+                fBill.Text = "₹" & finalAmount & "/-"
+
+
+
                 itemList.Items.RemoveAt(ind)
                 quantityList.Items.RemoveAt(ind)
+                gstAmt.Items.RemoveAt(ind)
+                totalPrice.Items.RemoveAt(ind)
+                priceList.Items.RemoveAt(ind)
             End If
         End If
 
     End Sub
 
     Private Sub Discount_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles Discount.KeyPress
-        If (Not Char.IsNumber(e.KeyChar) And Not e.KeyChar = Chr(Keys.Back) And Not e.KeyChar = Chr(Keys.Delete)) Then
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not e.KeyChar = "."c AndAlso Not e.KeyChar = Chr(Keys.Back) Then
             e.Handled = True
-            MessageBox.Show("Price must be in Numbers.")
+            MessageBox.Show("Price must be in Numbers.", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        ElseIf e.KeyChar = "."c AndAlso Discount.Text.Contains(".") Then
+            e.Handled = True
+            MessageBox.Show("Only one decimal point is allowed.", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        ElseIf Discount.Text = "" And e.KeyChar = "."c Then
+            e.Handled = True
+            MessageBox.Show("Enter Numeric Value!", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+    End Sub
+
+    Private Sub Discount_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Discount.TextChanged
+        If Discount.Text <> "" Then
+            If CSng(Discount.Text) > 100 Then
+                MessageBox.Show("Discount Should be less than 100%", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Else
+                Discount_Cal()
+            End If
+        End If
+    End Sub
+
+    Private Sub Discount_Cal()
+        Dim fAmount As Single
+        For Each i In totalPrice.Items  'calculating the total price
+            fAmount += CSng(i)
+        Next
+        If Discount.Text = "" Then
+            fBill.Text = "₹" & fAmount & "/-"
+        Else
+            Dim finalAmount = CSng(fAmount)
+            Dim discountRate = CSng(Discount.Text)
+            Dim disAmt = (finalAmount * discountRate) / 100     'calculating the discount
+
+            fBill.Text = "₹" & (finalAmount - disAmt) & "/-"
         End If
     End Sub
 End Class

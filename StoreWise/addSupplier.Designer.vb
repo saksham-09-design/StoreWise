ef4cf58c4eb1c79f30f127c452fb820d4f71e08c
Saksham <sakshamloona9@gmail.com>
2025-02-19 10:29:34 +0530

    added Add Supplier Page

---
+++ StoreWise/addSupplier.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class addSupplier
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.eMail = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.phone = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.supplierName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cancle = New System.Windows.Forms.Button()
+        Me.clear = New System.Windows.Forms.Button()
+        Me.save = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'eMail
+        '
+        Me.eMail.Font = New System.Drawing.Font("Now Medium", 15.8!)
+        Me.eMail.Location = New System.Drawing.Point(319, 263)
+        Me.eMail.Name = "eMail"
+        Me.eMail.Size = New System.Drawing.Size(470, 39)
+        Me.eMail.TabIndex = 15
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Now Medium", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Red
+        Me.Label6.Location = New System.Drawing.Point(41, 265)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(122, 34)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "E-Mail:"
+        '
+        'phone
+        '
+        Me.phone.Font = New System.Drawing.Font("Now Medium", 15.8!)
+        Me.phone.Location = New System.Drawing.Point(319, 189)
+        Me.phone.Name = "phone"
+        Me.phone.Size = New System.Drawing.Size(470, 39)
+        Me.phone.TabIndex = 14
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Now Medium", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Location = New System.Drawing.Point(41, 191)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(250, 34)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "*Phone Number:"
+        '
+        'supplierName
+        '
+        Me.supplierName.Font = New System.Drawing.Font("Now Medium", 15.8!)
+        Me.supplierName.Location = New System.Drawing.Point(319, 123)
+        Me.supplierName.Name = "supplierName"
+        Me.supplierName.Size = New System.Drawing.Size(470, 39)
+        Me.supplierName.TabIndex = 12
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Now Medium", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(41, 125)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(123, 34)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "*Name:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Now", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label1.Location = New System.Drawing.Point(263, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(279, 48)
+        Me.Label1.TabIndex = 77
+        Me.Label1.Text = "Add Supplier"
+        '
+        'cancle
+        '
+        Me.cancle.BackColor = System.Drawing.Color.Red
+        Me.cancle.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.cancle.Font = New System.Drawing.Font("Now", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cancle.ForeColor = System.Drawing.Color.Ivory
+        Me.cancle.Location = New System.Drawing.Point(539, 336)
+        Me.cancle.Name = "cancle"
+        Me.cancle.Size = New System.Drawing.Size(168, 52)
+        Me.cancle.TabIndex = 80
+        Me.cancle.Text = "&Cancel"
+        Me.cancle.UseVisualStyleBackColor = False
+        '
+        'clear
+        '
+        Me.clear.BackColor = System.Drawing.Color.Red
+        Me.clear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.clear.Font = New System.Drawing.Font("Now", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clear.ForeColor = System.Drawing.Color.Ivory
+        Me.clear.Location = New System.Drawing.Point(336, 336)
+        Me.clear.Name = "clear"
+        Me.clear.Size = New System.Drawing.Size(168, 52)
+        Me.clear.TabIndex = 79
+        Me.clear.Text = "Clear"
+        Me.clear.UseVisualStyleBackColor = False
+        '
+        'save
+        '
+        Me.save.BackColor = System.Drawing.Color.Red
+        Me.save.Font = New System.Drawing.Font("Now", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.save.ForeColor = System.Drawing.Color.Ivory
+        Me.save.Location = New System.Drawing.Point(134, 336)
+        Me.save.Name = "save"
+        Me.save.Size = New System.Drawing.Size(168, 52)
+        Me.save.TabIndex = 78
+        Me.save.Text = "&Save"
+        Me.save.UseVisualStyleBackColor = False
+        '
+        'addSupplier
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(876, 435)
+        Me.Controls.Add(Me.cancle)
+        Me.Controls.Add(Me.clear)
+        Me.Controls.Add(Me.save)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.eMail)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.phone)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.supplierName)
+        Me.Controls.Add(Me.Label2)
+        Me.Name = "addSupplier"
+        Me.Text = "Supplier"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents eMail As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents phone As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents supplierName As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cancle As System.Windows.Forms.Button
+    Friend WithEvents clear As System.Windows.Forms.Button
+    Friend WithEvents save As System.Windows.Forms.Button
+End Class
---
+++ StoreWise/addSupplier.vb
@@ -0,0 +1,19 @@
+﻿Public Class addSupplier
+
+    Private Sub clear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles clear.Click
+        supplierName.Text = ""
+        phone.Text = ""
+        eMail.Text = ""
+    End Sub
+
+    Private Sub phone_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles phone.KeyPress
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not e.KeyChar = Chr(Keys.Back) Then
+            e.Handled = True
+            MessageBox.Show("Phone must be in Numbers.", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+    End Sub
+
+    Private Sub phone_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles phone.TextChanged
+
+    End Sub
+End Class

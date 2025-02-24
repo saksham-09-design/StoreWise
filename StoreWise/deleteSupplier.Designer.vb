935a4f252fbd437111b4b233f6a422bc148e84c0
Saksham <sakshamloona9@gmail.com>
2025-02-24 17:27:01 +0530

    Added Database to Delete supplier

--- StoreWise/deleteSupplier.Designer.vb
+++ StoreWise/deleteSupplier.Designer.vb
@@ -22,30 +22,30 @@ Partial Class deleteSupplier
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.supplierNameList = New System.Windows.Forms.ComboBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.cancle = New System.Windows.Forms.Button()
-        Me.save = New System.Windows.Forms.Button()
+        Me.delete = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.eMail = New System.Windows.Forms.TextBox()
-        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
         Me.phone = New System.Windows.Forms.TextBox()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.supplierName = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'ComboBox1
+        'supplierNameList
         '
-        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ComboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
-        Me.ComboBox1.Font = New System.Drawing.Font("Now Medium", 15.0!)
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"Sunfeast", "Cremica", "Nestle"})
-        Me.ComboBox1.Location = New System.Drawing.Point(344, 189)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(470, 38)
-        Me.ComboBox1.TabIndex = 92
+        Me.supplierNameList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.supplierNameList.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.supplierNameList.Font = New System.Drawing.Font("Now Medium", 15.0!)
+        Me.supplierNameList.FormattingEnabled = True
+        Me.supplierNameList.Items.AddRange(New Object() {"Sunfeast", "Cremica", "Nestle"})
+        Me.supplierNameList.Location = New System.Drawing.Point(344, 189)
+        Me.supplierNameList.Name = "supplierNameList"
+        Me.supplierNameList.Size = New System.Drawing.Size(470, 38)
+        Me.supplierNameList.TabIndex = 92
         '
         'Label3
         '
@@ -54,7 +54,7 @@ Partial Class deleteSupplier
         Me.Label3.ForeColor = System.Drawing.Color.Red
         Me.Label3.Location = New System.Drawing.Point(66, 195)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(137, 32)
+        Me.Label3.Size = New System.Drawing.Size(136, 32)
         Me.Label3.TabIndex = 93
         Me.Label3.Text = "Supplier:"
         '
@@ -71,17 +71,17 @@ Partial Class deleteSupplier
         Me.cancle.Text = "&Cancel"
         Me.cancle.UseVisualStyleBackColor = False
         '
-        'save
+        'delete
         '
-        Me.save.BackColor = System.Drawing.Color.Red
-        Me.save.Font = New System.Drawing.Font("Now", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.save.ForeColor = System.Drawing.Color.Ivory
-        Me.save.Location = New System.Drawing.Point(159, 470)
-        Me.save.Name = "save"
-        Me.save.Size = New System.Drawing.Size(168, 52)
-        Me.save.TabIndex = 90
-        Me.save.Text = "Delete"
-        Me.save.UseVisualStyleBackColor = False
+        Me.delete.BackColor = System.Drawing.Color.Red
+        Me.delete.Font = New System.Drawing.Font("Now", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.delete.ForeColor = System.Drawing.Color.Ivory
+        Me.delete.Location = New System.Drawing.Point(159, 470)
+        Me.delete.Name = "delete"
+        Me.delete.Size = New System.Drawing.Size(168, 52)
+        Me.delete.TabIndex = 90
+        Me.delete.Text = "Delete"
+        Me.delete.UseVisualStyleBackColor = False
         '
         'Label1
         '
@@ -103,16 +103,16 @@ Partial Class deleteSupplier
         Me.eMail.Size = New System.Drawing.Size(470, 39)
         Me.eMail.TabIndex = 86
         '
-        'Label6
+        'Label4
         '
-        Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Now Medium", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.ForeColor = System.Drawing.Color.Red
-        Me.Label6.Location = New System.Drawing.Point(66, 399)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(122, 34)
-        Me.Label6.TabIndex = 88
-        Me.Label6.Text = "E-Mail:"
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Now Medium", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(66, 399)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(122, 34)
+        Me.Label4.TabIndex = 88
+        Me.Label4.Text = "E-Mail:"
         '
         'phone
         '
@@ -159,13 +159,13 @@ Partial Class deleteSupplier
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(881, 612)
-        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.supplierNameList)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.cancle)
-        Me.Controls.Add(Me.save)
+        Me.Controls.Add(Me.delete)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.eMail)
-        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.phone)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.supplierName)
@@ -176,13 +176,13 @@ Partial Class deleteSupplier
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents supplierNameList As System.Windows.Forms.ComboBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents cancle As System.Windows.Forms.Button
-    Friend WithEvents save As System.Windows.Forms.Button
+    Friend WithEvents delete As System.Windows.Forms.Button
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents eMail As System.Windows.Forms.TextBox
-    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents phone As System.Windows.Forms.TextBox
     Friend WithEvents Label5 As System.Windows.Forms.Label
     Friend WithEvents supplierName As System.Windows.Forms.TextBox
--- StoreWise/deleteSupplier.vb
+++ StoreWise/deleteSupplier.vb
@@ -1,3 +1,99 @@
 ﻿Public Class deleteSupplier
+    Dim sid As Integer
 
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles supplierNameList.SelectedIndexChanged
+        'fetching supplier details
+        Dim conn As New OleDb.OleDbConnection
+        conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Saksham\Documents\StoreWise.accdb"
+        If conn.State = ConnectionState.Closed Then
+            conn.Open()
+        End If
+        Dim sql As String = "select * from supplierTable where sName='" & supplierNameList.Text & "'"
+        Dim cmd As New OleDb.OleDbCommand(sql, conn)
+        Dim da As OleDb.OleDbDataAdapter
+        da = New OleDb.OleDbDataAdapter(cmd)
+        Dim dt As New DataTable
+        dt.Clear()
+        da.Fill(dt)
+        If dt.Rows.Count > 0 Then
+            supplierName.Text = dt.Rows(0).Item(1)
+            phone.Text = dt.Rows(0).Item(2)
+            eMail.Text = dt.Rows(0).Item(3)
+            sid = dt.Rows(0).Item(0)
+        Else
+            MessageBox.Show("No Supplier found", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+        conn.Close()
+    End Sub
+    Sub update_List()
+        Dim conn As New OleDb.OleDbConnection
+        conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Saksham\Documents\StoreWise.accdb"
+        If conn.State = ConnectionState.Closed Then
+            conn.Open()
+        End If
+        Dim sql As String = "select sName from supplierTable"
+        Dim cmd As New OleDb.OleDbCommand(sql, conn)
+        Dim da As OleDb.OleDbDataAdapter
+        da = New OleDb.OleDbDataAdapter(cmd)
+        Dim dt As New DataTable
+        dt.Clear()
+        da.Fill(dt)
+        supplierNameList.DataSource = dt
+        supplierNameList.DisplayMember = "sName"
+        conn.Close()
+    End Sub
+
+    Private Sub deleteSupplier_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            update_List()
+        Catch ex As Exception
+            MessageBox.Show("No Supplier Found", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        'setting up dark mode
+        If (DMode) Then
+            Me.BackColor = Color.FromArgb(255, 13, 17, 23)
+            For i = 1 To 5
+                Dim lbl As Label = Me.Controls("Label" & i)
+                lbl.ForeColor = Color.FromArgb(255, 240, 246, 252)
+            Next
+            delete.BackColor = Color.FromArgb(255, 31, 111, 235)
+            delete.BackColor = Color.FromArgb(255, 31, 111, 235)
+            cancle.BackColor = Color.FromArgb(255, 31, 111, 235)
+            cancle.BackColor = Color.FromArgb(255, 31, 111, 235)
+            cancle.BackColor = Color.FromArgb(255, 31, 111, 235)
+            supplierName.BackColor = Color.FromArgb(255, 33, 40, 48)
+            supplierName.ForeColor = Color.FromArgb(255, 240, 246, 252)
+            phone.BackColor = Color.FromArgb(255, 33, 40, 48)
+            phone.ForeColor = Color.FromArgb(255, 240, 246, 252)
+            eMail.BackColor = Color.FromArgb(255, 33, 40, 48)
+            eMail.ForeColor = Color.FromArgb(255, 240, 246, 252)
+            supplierNameList.BackColor = Color.FromArgb(255, 33, 40, 48)
+            supplierNameList.ForeColor = Color.FromArgb(255, 240, 246, 252)
+        End If
+    End Sub
+
+    Private Sub delete_Click(sender As Object, e As EventArgs) Handles delete.Click
+
+        If supplierName.Text.Trim = "" Then
+            MessageBox.Show("No Supplier to delete", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        Dim conn As New OleDb.OleDbConnection
+        conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Saksham\Documents\StoreWise.accdb"
+        If conn.State = ConnectionState.Closed Then
+            conn.Open()
+        End If
+        Dim sql As String = "delete from supplierTable where ID =" & sid
+        Dim cmd As New OleDb.OleDbCommand(sql, conn)
+        If cmd.ExecuteNonQuery() <> 0 Then
+            MessageBox.Show("Supplier Deleted Successfully", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Try
+                update_List()
+            Catch ex As Exception
+                MessageBox.Show("No Supplier Found", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+        conn.Close()
+    End Sub
 End Class

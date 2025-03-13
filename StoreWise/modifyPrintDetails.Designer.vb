8012b0c7a1bafb5c99751f0319c7890ea244652e
Saksham <sakshamloona9@gmail.com>
2025-03-13 23:30:05 +0530

    Added modify Print details

---
+++ StoreWise/modifyPrintDetails.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class modifyPrintDetails
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.t4 = New System.Windows.Forms.TextBox()
+        Me.t3 = New System.Windows.Forms.TextBox()
+        Me.t2 = New System.Windows.Forms.TextBox()
+        Me.t1 = New System.Windows.Forms.TextBox()
+        Me.cancle = New System.Windows.Forms.Button()
+        Me.sFormat = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.add = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.mail = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.phone = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.fName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        't4
+        '
+        Me.t4.Font = New System.Drawing.Font("Now Medium", 15.8!)
+        Me.t4.Location = New System.Drawing.Point(233, 517)
+        Me.t4.Name = "t4"
+        Me.t4.Size = New System.Drawing.Size(717, 39)
+        Me.t4.TabIndex = 42
+        '
+        't3
+        '
+        Me.t3.Font = New System.Drawing.Font("Now Medium", 15.8!)
+        Me.t3.Location = New System.Drawing.Point(233, 465)
+        Me.t3.Name = "t3"
+        Me.t3.Size = New System.Drawing.Size(717, 39)
+        Me.t3.TabIndex = 41
+        '
+        't2
+        '
+        Me.t2.Font = New System.Drawing.Font("Now Medium", 15.8!)
+        Me.t2.Location = New System.Drawing.Point(233, 409)
+        Me.t2.Name = "t2"
+        Me.t2.Size = New System.Drawing.Size(717, 39)
+        Me.t2.TabIndex = 40
+        '
+        't1
+        '
+        Me.t1.Font = New System.Drawing.Font("Now Medium", 15.8!)
+        Me.t1.Location = New System.Drawing.Point(233, 358)
+        Me.t1.Name = "t1"
+        Me.t1.Size = New System.Drawing.Size(717, 39)
+        Me.t1.TabIndex = 39
+        '
+        'cancle
+        '
+        Me.cancle.BackColor = System.Drawing.Color.Red
+        Me.cancle.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.cancle.Font = New System.Drawing.Font("Now", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cancle.ForeColor = System.Drawing.Color.Ivory
+        Me.cancle.Location = New System.Drawing.Point(559, 585)
+        Me.cancle.Name = "cancle"
+        Me.cancle.Size = New System.Drawing.Size(168, 52)
+        Me.cancle.TabIndex = 38
+        Me.cancle.Text = "Cancel"
+        Me.cancle.UseVisualStyleBackColor = False
+        '
+        'sFormat
+        '
+        Me.sFormat.BackColor = System.Drawing.Color.Red
+        Me.sFormat.Font = New System.Drawing.Font("Now", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.sFormat.ForeColor = System.Drawing.Color.Ivory
+        Me.sFormat.Location = New System.Drawing.Point(329, 585)
+        Me.sFormat.Name = "sFormat"
+        Me.sFormat.Size = New System.Drawing.Size(168, 52)
+        Me.sFormat.TabIndex = 37
+        Me.sFormat.Text = "Save"
+        Me.sFormat.UseVisualStyleBackColor = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Now", 16.0!, System.Drawing.FontStyle.Bold)
+        Me.Label6.ForeColor = System.Drawing.Color.Red
+        Me.Label6.Location = New System.Drawing.Point(33, 360)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(100, 32)
+        Me.Label6.TabIndex = 36
+        Me.Label6.Text = "Terms:"
+        '
+        'add
+        '
+        Me.add.Font = New System.Drawing.Font("Now Medium", 15.8!)
+        Me.add.Location = New System.Drawing.Point(233, 287)
+        Me.add.Name = "add"
+        Me.add.Size = New System.Drawing.Size(717, 39)
+        Me.add.TabIndex = 35
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Now", 16.0!, System.Drawing.FontStyle.Bold)
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Location = New System.Drawing.Point(33, 294)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(131, 32)
+        Me.Label5.TabIndex = 34
+        Me.Label5.Text = "Address:"
+        '
+        'mail
+        '
+        Me.mail.Font = New System.Drawing.Font("Now Medium", 15.8!)
+        Me.mail.Location = New System.Drawing.Point(233, 227)
+        Me.mail.Name = "mail"
+        Me.mail.Size = New System.Drawing.Size(717, 39)
+        Me.mail.TabIndex = 33
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Now", 16.0!, System.Drawing.FontStyle.Bold)
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(33, 234)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(114, 32)
+        Me.Label4.TabIndex = 32
+        Me.Label4.Text = "E-Mail:"
+        '
+        'phone
+        '
+        Me.phone.Font = New System.Drawing.Font("Now Medium", 15.8!)
+        Me.phone.Location = New System.Drawing.Point(233, 166)
+        Me.phone.Name = "phone"
+        Me.phone.Size = New System.Drawing.Size(717, 39)
+        Me.phone.TabIndex = 31
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Now", 16.0!, System.Drawing.FontStyle.Bold)
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(33, 173)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(157, 32)
+        Me.Label3.TabIndex = 30
+        Me.Label3.Text = "Phone No:"
+        '
+        'fName
+        '
+        Me.fName.Font = New System.Drawing.Font("Now Medium", 15.8!)
+        Me.fName.Location = New System.Drawing.Point(233, 107)
+        Me.fName.Name = "fName"
+        Me.fName.Size = New System.Drawing.Size(717, 39)
+        Me.fName.TabIndex = 29
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Now", 16.0!, System.Drawing.FontStyle.Bold)
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(33, 114)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(172, 32)
+        Me.Label2.TabIndex = 28
+        Me.Label2.Text = "Firm Name:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Now", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label1.Location = New System.Drawing.Point(297, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(345, 48)
+        Me.Label1.TabIndex = 27
+        Me.Label1.Text = "Bill Print Details"
+        '
+        'modifyPrintDetails
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(999, 673)
+        Me.Controls.Add(Me.t4)
+        Me.Controls.Add(Me.t3)
+        Me.Controls.Add(Me.t2)
+        Me.Controls.Add(Me.t1)
+        Me.Controls.Add(Me.cancle)
+        Me.Controls.Add(Me.sFormat)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.add)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.mail)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.phone)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.fName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "modifyPrintDetails"
+        Me.Text = "modifyPrintDetails"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents t4 As TextBox
+    Friend WithEvents t3 As TextBox
+    Friend WithEvents t2 As TextBox
+    Friend WithEvents t1 As TextBox
+    Friend WithEvents cancle As Button
+    Friend WithEvents sFormat As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents add As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents mail As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents phone As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents fName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ StoreWise/modifyPrintDetails.vb
@@ -0,0 +1,73 @@
+﻿Imports System.ComponentModel.DataAnnotations
+
+Public Class modifyPrintDetails
+    Dim firmName As String = ""
+    Private Sub modifyPrintDetails_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim con As New OleDb.OleDbConnection
+        con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Saksham\Documents\StoreWise.accdb"
+        Try
+            If con.State = ConnectionState.Closed Then
+                con.Open()
+            End If
+            Dim sql As String = "select * from billFormat"
+            Dim da As New OleDb.OleDbDataAdapter(sql, con)
+            Dim dt As New DataTable
+            da.Fill(dt)
+            firmName = CStr(dt.Rows(0).Item(0))
+            fName.Text = CStr(dt.Rows(0).Item(0))
+            phone.Text = CStr(dt.Rows(0).Item(1))
+            mail.Text = CStr(dt.Rows(0).Item(2))
+            add.Text = CStr(dt.Rows(0).Item(3))
+            t1.Text = CStr(dt.Rows(0).Item(4))
+            t2.Text = CStr(dt.Rows(0).Item(5))
+            t3.Text = CStr(dt.Rows(0).Item(6))
+            t4.Text = CStr(dt.Rows(0).Item(7))
+        Catch ex As Exception
+            MessageBox.Show("Error " & ex.ToString, "Storewise", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            con.Close()
+        End Try
+
+        'setting up dark mode
+
+    End Sub
+
+    Private Sub mail_KeyPress(sender As Object, e As KeyPressEventArgs) Handles mail.KeyPress
+        If e.KeyChar = "@"c AndAlso mail.Text.Contains("@") Then
+            e.Handled = True
+            MessageBox.Show("Only one @ is allowed.", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+    End Sub
+
+    Private Sub phone_KeyPress(sender As Object, e As KeyPressEventArgs) Handles phone.KeyPress
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not e.KeyChar = Chr(Keys.Back) Then
+            e.Handled = True
+            MessageBox.Show("Phone must be in Numbers.", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+    End Sub
+
+    Private Sub sFormat_Click(sender As Object, e As EventArgs) Handles sFormat.Click
+        If (fName.Text.Trim = "" Or phone.Text.Trim = "" Or mail.Text.Trim = "" Or add.Text.Trim = "" Or t1.Text.Trim = "" Or t2.Text.Trim = "" Or t3.Text.Trim = "" Or t4.Text.Trim = "") Then
+            MessageBox.Show("Please Fill All Details!", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf ((mail.Text.Trim.EndsWith(".com") = False) Or (mail.Text.Trim.Contains("@") = False)) Then
+            MessageBox.Show("Invalid Email", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            Dim con As New OleDb.OleDbConnection
+            con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Saksham\Documents\StoreWise.accdb"
+            'modifing bill info in database
+            Try
+                If con.State = ConnectionState.Closed Then
+                    con.Open()
+                End If
+                Dim sql = "Update billFormat set fName = '" & fName.Text.Trim & "', phone = '" & phone.Text.Trim & "', eMail = '" & mail.Text.Trim & "', [add] = '" & add.Text.Trim & "', t1 = '" & t1.Text.Trim & "', t2 = '" & t2.Text.Trim & "', t3 = '" & t3.Text.Trim & "', t4 = '" & t4.Text.Trim & "' where fName = '" & firmName & "'"
+                Dim cmd As New OleDb.OleDbCommand(sql, con)
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Format Modified Successfully!", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show("Error:" & ex.ToString(), "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Finally
+                con.Close()
+            End Try
+        End If
+    End Sub
+End Class

abdc83b5b2f6700a00f690492e9151aa11582df4
Saksham <sakshamloona9@gmail.com>
2025-04-17 10:51:04 +0530

    Added Receivables

---
+++ StoreWise/receivableSale.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class receivableSale
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.customerName = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.toDate = New System.Windows.Forms.DateTimePicker()
+        Me.fromDate = New System.Windows.Forms.DateTimePicker()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.filter = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Now", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label1.Location = New System.Drawing.Point(353, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(498, 48)
+        Me.Label1.TabIndex = 83
+        Me.Label1.Text = "Credit Sale Transaction"
+        '
+        'customerName
+        '
+        Me.customerName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.customerName.Enabled = False
+        Me.customerName.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.customerName.Font = New System.Drawing.Font("Now Medium", 15.0!)
+        Me.customerName.FormattingEnabled = True
+        Me.customerName.Items.AddRange(New Object() {"Ram", "Sham", "Rahul", "Anshika", "Sneha", "Rohit", "Piyush", "Manan", "Kashish"})
+        Me.customerName.Location = New System.Drawing.Point(292, 215)
+        Me.customerName.Name = "customerName"
+        Me.customerName.Size = New System.Drawing.Size(903, 38)
+        Me.customerName.TabIndex = 95
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Now Medium", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(25, 217)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(261, 34)
+        Me.Label2.TabIndex = 94
+        Me.Label2.Text = "Customer Name:"
+        '
+        'toDate
+        '
+        Me.toDate.Enabled = False
+        Me.toDate.Font = New System.Drawing.Font("Now Medium", 12.8!)
+        Me.toDate.Location = New System.Drawing.Point(776, 155)
+        Me.toDate.Name = "toDate"
+        Me.toDate.Size = New System.Drawing.Size(419, 33)
+        Me.toDate.TabIndex = 93
+        '
+        'fromDate
+        '
+        Me.fromDate.Enabled = False
+        Me.fromDate.Font = New System.Drawing.Font("Now Medium", 12.8!)
+        Me.fromDate.Location = New System.Drawing.Point(178, 155)
+        Me.fromDate.Name = "fromDate"
+        Me.fromDate.Size = New System.Drawing.Size(419, 33)
+        Me.fromDate.TabIndex = 92
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Now Medium", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(696, 154)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 34)
+        Me.Label4.TabIndex = 91
+        Me.Label4.Text = "To:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Now Medium", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Red
+        Me.Label5.Location = New System.Drawing.Point(25, 156)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(97, 34)
+        Me.Label5.TabIndex = 90
+        Me.Label5.Text = "From:"
+        '
+        'filter
+        '
+        Me.filter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.filter.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.filter.Font = New System.Drawing.Font("Now Medium", 15.0!)
+        Me.filter.FormattingEnabled = True
+        Me.filter.Items.AddRange(New Object() {"All", "Today", "Yesterday", "Past 7 days", "Past 1 Month", "Past 1 Year", "Custom Dates", "Customer Name"})
+        Me.filter.Location = New System.Drawing.Point(178, 92)
+        Me.filter.Name = "filter"
+        Me.filter.Size = New System.Drawing.Size(1017, 38)
+        Me.filter.TabIndex = 89
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Now", 16.0!, System.Drawing.FontStyle.Bold)
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(25, 92)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(94, 32)
+        Me.Label3.TabIndex = 88
+        Me.Label3.Text = "Filter:"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(31, 288)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Now Medium", 13.8!, System.Drawing.FontStyle.Bold)
+        Me.DataGridView1.RowsDefaultCellStyle = DataGridViewCellStyle2
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(1164, 410)
+        Me.DataGridView1.TabIndex = 87
+        '
+        'receivableSale
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1221, 733)
+        Me.Controls.Add(Me.customerName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.toDate)
+        Me.Controls.Add(Me.fromDate)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.filter)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "receivableSale"
+        Me.Text = "receivableSale"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents customerName As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents toDate As DateTimePicker
+    Friend WithEvents fromDate As DateTimePicker
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents filter As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ StoreWise/receivableSale.vb
@@ -0,0 +1,197 @@
+﻿Public Class receivableSale
+
+
+    Private Sub itemName_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles filter.SelectedIndexChanged
+
+        'fetching according to filters
+        If filter.Text = "Custom Dates" Then
+            Dim dateFrom As Date = CDate(Format(toDate.Value, "Short Date"))
+            Dim dateTo As Date = CDate(Format(fromDate.Value, "Short Date"))
+            fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit' and tDate >= #" & dateFrom & "# and tDate <= #" & dateTo & "#")
+            fromDate.Enabled = True
+            toDate.Enabled = True
+            customerName.Enabled = False
+        ElseIf filter.Text = "Customer Name" Then
+            customerName.Enabled = True
+            fromDate.Enabled = False
+            toDate.Enabled = False
+        ElseIf filter.Text = "Today" Then
+            Dim dateT As Date = CDate(Format(Now(), "Short Date"))
+            fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit' and tDate = #" & dateT & "#")
+            fromDate.Enabled = False
+            toDate.Enabled = False
+            customerName.Enabled = False
+        ElseIf filter.Text = "Yesterday" Then
+            Dim dateT As Date = CDate(Format(Now().AddDays(-1), "Short Date"))
+            fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit' and tDate = #" & dateT & "#")
+            fromDate.Enabled = False
+            toDate.Enabled = False
+            customerName.Enabled = False
+        ElseIf filter.Text = "Past 7 days" Then
+            Dim dateT As Date = CDate(Format(Now().AddDays(-7), "Short Date"))
+            fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit' and tDate >= #" & dateT & "#")
+            fromDate.Enabled = False
+            toDate.Enabled = False
+            customerName.Enabled = False
+        ElseIf filter.Text = "Past 1 Month" Then
+            Dim dateT As Date = CDate(Format(Now().AddMonths(-1), "Short Date"))
+            fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit' and tDate >= #" & dateT & "#")
+            fromDate.Enabled = False
+            toDate.Enabled = False
+            customerName.Enabled = False
+        ElseIf filter.Text = "Past 1 Year" Then
+            Dim dateT As Date = CDate(Format(Now().AddYears(-1), "Short Date"))
+            fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit' and tDate >= #" & dateT & "#")
+            fromDate.Enabled = False
+            toDate.Enabled = False
+            customerName.Enabled = False
+        Else
+            fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit'")
+            fromDate.Enabled = False
+            toDate.Enabled = False
+            customerName.Enabled = False
+        End If
+    End Sub
+
+
+    Private Sub receivableSale_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim conn As New OleDb.OleDbConnection
+        conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Saksham\Documents\StoreWise.accdb"
+
+
+        'updating the customer list
+        Try
+            If conn.State = ConnectionState.Closed Then
+                conn.Open()
+            End If
+            Dim sql As String = "SELECT DISTINCT cName FROM saleTable"
+            Dim cmd As New OleDb.OleDbCommand(sql, conn)
+            Dim da As New OleDb.OleDbDataAdapter(cmd)
+            Dim dt As New DataTable
+            da.Fill(dt)
+            customerName.DataSource = dt
+            customerName.DisplayMember = "cName"
+            If dt.Rows.Count = 0 Then
+                MessageBox.Show("No Customer found", "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            conn.Close()
+        End Try
+
+
+        'setting up data grid
+        fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit'")
+
+        'setting up dark mode
+        If DMode Then
+            Me.BackColor = Color.FromArgb(255, 13, 17, 23)
+            For i = 1 To 5
+                Dim lbl As Label = Me.Controls("Label" & i)
+                lbl.ForeColor = Color.FromArgb(255, 240, 246, 252)
+            Next
+            DataGridView1.BackgroundColor = Color.FromArgb(255, 13, 17, 23)
+            DataGridView1.DefaultCellStyle.BackColor = Color.FromArgb(255, 13, 17, 23)
+            DataGridView1.DefaultCellStyle.ForeColor = Color.FromArgb(255, 240, 246, 252)
+            DataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(255, 13, 17, 23)
+            DataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.FromArgb(255, 240, 246, 252)
+            DataGridView1.GridColor = Color.FromArgb(255, 240, 246, 252)
+            filter.BackColor = Color.FromArgb(255, 13, 17, 23)
+            filter.ForeColor = Color.FromArgb(255, 240, 246, 252)
+            customerName.BackColor = Color.FromArgb(255, 13, 17, 23)
+            customerName.ForeColor = Color.FromArgb(255, 240, 246, 252)
+            fromDate.BackColor = Color.FromArgb(255, 13, 17, 23)
+            fromDate.ForeColor = Color.FromArgb(255, 240, 246, 252)
+            fromDate.CalendarMonthBackground = Color.FromArgb(255, 13, 17, 23)
+            fromDate.CalendarTitleBackColor = Color.FromArgb(255, 13, 17, 23)
+            fromDate.CalendarTitleForeColor = Color.FromArgb(255, 240, 246, 252)
+            fromDate.CalendarTrailingForeColor = Color.FromArgb(255, 240, 246, 252)
+            fromDate.CalendarForeColor = Color.FromArgb(255, 240, 246, 252)
+            toDate.BackColor = Color.FromArgb(255, 13, 17, 23)
+            toDate.ForeColor = Color.FromArgb(255, 240, 246, 252)
+            toDate.CalendarMonthBackground = Color.FromArgb(255, 13, 17, 23)
+            toDate.CalendarTitleBackColor = Color.FromArgb(255, 13, 17, 23)
+            toDate.CalendarTitleForeColor = Color.FromArgb(255, 240, 246, 252)
+            toDate.CalendarTrailingForeColor = Color.FromArgb(255, 240, 246, 252)
+            toDate.CalendarForeColor = Color.FromArgb(255, 240, 246, 252)
+        End If
+    End Sub
+
+    Sub fetch_data_UG(sqlI As String)
+        Dim conn As New OleDb.OleDbConnection
+        conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Saksham\Documents\StoreWise.accdb"
+        Try
+            Dim sql As String = sqlI
+            Dim da As New OleDb.OleDbDataAdapter(sql, conn)
+            Dim dt As New DataTable
+            dt.Clear()
+            da.Fill(dt)
+            DataGridView1.DataSource = dt
+            DataGridView1.Columns(0).Width = 100
+            DataGridView1.Columns(1).Width = 270
+            DataGridView1.Columns(2).Width = 160
+            DataGridView1.Columns(3).Width = 160
+            DataGridView1.Columns(4).Width = 160
+            DataGridView1.Columns(5).Width = 160
+
+
+            DataGridView1.Columns(0).HeaderText = "Transaction ID"
+            DataGridView1.Columns(1).HeaderText = "Customer Name"
+            DataGridView1.Columns(2).HeaderText = "Date"
+            DataGridView1.Columns(3).HeaderText = "Phone Number"
+            DataGridView1.Columns(4).HeaderText = "Type"
+            DataGridView1.Columns(4).HeaderText = "Amount"
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.ToString, "Store Wise", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub customerName_SelectedIndexChanged(sender As Object, e As EventArgs) Handles customerName.SelectedIndexChanged
+        fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit' and cName = '" & customerName.Text & "'")
+    End Sub
+
+    Private Sub fromDate_ValueChanged(sender As Object, e As EventArgs) Handles fromDate.ValueChanged
+        Dim dateFrom As Date = CDate(Format(toDate.Value, "Short Date"))
+        Dim dateTo As Date = CDate(Format(fromDate.Value, "Short Date"))
+        fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit' and pDate >= #" & dateFrom & "# and pDate <= #" & dateTo & "#")
+    End Sub
+
+    Private Sub toDate_ValueChanged(sender As Object, e As EventArgs) Handles toDate.ValueChanged
+        Dim dateFrom As Date = CDate(Format(toDate.Value, "Short Date"))
+        Dim dateTo As Date = CDate(Format(fromDate.Value, "Short Date"))
+        fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit' and pDate >= #" & dateFrom & "# and pDate <= #" & dateTo & "#")
+    End Sub
+
+    Private Sub DataGridView1_RowHeaderMouseDoubleClick(sender As Object, e As DataGridViewCellMouseEventArgs) Handles DataGridView1.RowHeaderMouseDoubleClick
+        Dim recSalId = DataGridView1.Rows(e.RowIndex).Cells(0).Value
+        If MessageBox.Show("Payment Received for this Transactio?", "StoreWise", MessageBoxButtons.YesNo, MessageBoxIcon.Information) = DialogResult.Yes Then
+            Dim con As New OleDb.OleDbConnection
+            con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Saksham\Documents\StoreWise.accdb"
+            Try
+                If con.State = ConnectionState.Closed Then
+                    con.Open()
+                End If
+
+                Dim sql As String = "Update saleTable set tType = 'Cash' where ID = " & recSalId
+                Dim cmd As New OleDb.OleDbCommand(sql, con)
+                If cmd.ExecuteNonQuery() Then
+                    MessageBox.Show("Changes Done", "StoreWise", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit'")
+                End If
+            Catch ex As Exception
+                MessageBox.Show("Error: " & ex.Message, "Storewise", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Sub sales_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        fetch_data_UG("select ID, cName, tDate, phoneNumber, tType, bAmt from saleTable where tType = 'Credit'")
+    End Sub
+
+
+End Class
